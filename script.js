1f04c0a25c5191a4078ba2dd5e6c927382ca698d
Riona Westphal <riona.westphal@gmail.com>
2022-07-25 22:03:02 +0900

    Add button which prompts user to change grid size
    A button at the top of the page prompts the user to input a new grid
    size. This then deletes the previous grid and replaces it with the new
    grid. The new grid takes up the same amount of space as the previous
    grid.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function makeGrid() {
+const button = document.getElementById("changeGrid");
+button.addEventListener("click",promptSize);
+const maxWidth = 520;
+const maxHeight = 520;
+let gridSize = 16;
+makeGrid(gridSize);
+
+function makeGrid(gridSize) {
     let container = document.getElementById("grid");
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < gridSize; i++) {
         let row = document.createElement("div");
         row.style.display = "flex";
-        for(let j = 0; j < 16; j++) {
+        for(let j = 0; j < gridSize; j++) {
             let cell = document.createElement("div");
-            cell.style.width = "40px";
-            cell.style.height = "40px";
+            cell.style.width = `${maxWidth/gridSize}px`;
+            cell.style.height = `${maxHeight/gridSize}px`;
             cell.style.background = "gray";
             cell.style.borderStyle = "solid";
             cell.style.borderColor = "black";
@@ -25,7 +32,17 @@ function changeColor(e) {
 
 function revertColor(e) {
     e.fromElement.style.backgroundColor="gray";
-    console.log(e);
 }
 
-makeGrid();
+function promptSize(e) {
+    gridSize = prompt("Input your desired grid size:");
+    if(gridSize <= 100) {
+        removeGrid();
+        makeGrid(gridSize);
+    }
+}
+
+function removeGrid()
+{
+    document.getElementById("grid").innerHTML = "";
+}
